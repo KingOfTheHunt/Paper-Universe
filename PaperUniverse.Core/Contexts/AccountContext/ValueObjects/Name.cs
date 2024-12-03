5f26a81c92a38852f94ecc833304f75785d0e900
Davi Francisco <davifrancisco214@outlook.com>
2024-12-03 07:34:58 -0300

    Criação e testes do Name

---
+++ PaperUniverse.Core/Contexts/AccountContext/ValueObjects/Name.cs
@@ -0,0 +1,21 @@
+using Flunt.Validations;
+using PaperUniverse.Core.Contexts.SharedContext.ValueObjects;
+
+namespace PaperUniverse.Core.Contexts.AccountContext.ValueObjects;
+
+public class Name : ValueObject
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    public Name(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        
+        AddNotifications(new Contract<Name>()
+            .Requires()
+            .IsNotNullOrEmpty(FirstName, "FirstName", "O primeiro nome precisa ser informado.")
+            .IsNotNullOrEmpty(LastName, "LastName", "O último nome precisa ser informado."));
+    }
+}
