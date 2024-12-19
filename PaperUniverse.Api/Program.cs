cc81aa7fd04672fcbab8e17dfbaff023596c7089
Davi Francisco <davifrancisco214@outlook.com>
2024-12-19 07:20:11 -0300

    Configuração do swagger para usar o nome completo como id dos schemas

--- PaperUniverse.Api/Program.cs
+++ PaperUniverse.Api/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(y => y.FullName);
+});
 builder.AddConfiguration();
 builder.AddDatabase();
 builder.AddAccountContext();
