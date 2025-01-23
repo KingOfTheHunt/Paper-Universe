eb27c5c43c5416012aba8d37271aa2ad5e54df2f
Davi Francisco <davifrancisco214@outlook.com>
2025-01-22 21:36:41 -0300

    Ocultação do campo Email do Request do caso de uso Update Name

--- PaperUniverse.Core/Contexts/AccountContext/UseCases/UpdateName/Request.cs
+++ PaperUniverse.Core/Contexts/AccountContext/UseCases/UpdateName/Request.cs
@@ -1,9 +1,11 @@
+using System.Text.Json.Serialization;
 using MediatR;
 
 namespace PaperUniverse.Core.Contexts.AccountContext.UseCases.UpdateName;
 
 public class Request : IRequest<Response>
 {
+    [JsonIgnore]
     public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
